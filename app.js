76c2087423705cc5cf90ec6ed78397bf3b864063
arkarmoe <arkarmoe17@gmail.com>
2022-01-20 16:41:07 +0630

    app js update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8082;
 
 //routers
-const tokenRouter = require('./controllers/token');
+const tokenRouter = require('./apis/token');
 app.use('/access-token',tokenRouter)
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
